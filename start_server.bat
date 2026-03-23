1997e61f7855c5647773c1dcf2a05a2bd990d6be
MONISHASTRO <monish2004007@gmail.com>
2026-03-23 23:50:52 +0530

    Fix sidebar layout toggle and clean merge conflicts

--- start_server.bat
+++ start_server.bat
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 @echo off
 cd /d "c:\Users\HP\OneDrive\OneDrive_2026-03-23\stp viewer\stp-viewer-offline"
 python -m http.server 8000
-=======
-@echo off
-cd /d "c:\Users\HP\OneDrive\OneDrive_2026-03-23\stp viewer\stp-viewer-offline"
-python -m http.server 8000
->>>>>>> 110eaba (Update viewer UI + sidebar toggle + webassembly headers)
 pause
